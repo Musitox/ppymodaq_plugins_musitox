7f7dd1ff06251c95195f2559a25e12575d9e164e
pierre <bebop.bountyhunter@proton.me>
2026-06-29 16:44:15 +0200

    in rotation_tomo finish the hardware API of hte rotation

--- src/pymodaq_plugins_musitox/hardware/rotation_tomo.py
+++ src/pymodaq_plugins_musitox/hardware/rotation_tomo.py
@@ -3,13 +3,14 @@
 
 #Author : Pierre Piault for 3SR -- Grenoble // CNRS laboratory
 
-import numpy as np
+
 
 from pymodaq.utils.math_utils import gauss1D
 from typing import List, Union
 from collections.abc import Iterable
 from numbers import Number
 import math
+import numpy as np
 from time import perf_counter
 
 class Rotation_tomo:
@@ -27,6 +28,7 @@ class Rotation_tomo:
         self._epsilon = 0.01 #margin of position error
         #self._tau = 2   #time to reach the position
         self._moving = False
+        self._position = 10
 
     def open_communication(self):
         #connect the device
@@ -53,4 +55,43 @@ class Rotation_tomo:
         else:
             self._tau = value
 
+
+    def set_position(self, value, set_type='abs'):
+        """Move the rotation to the position
+        set_type should be 'abs' if absolute mouvment
+        or not 'abs' it is relative mouvment
+        """
+
+        if set_type == 'abs' and value < 0:
+            raise ValueError('current mock rotation cannot be negative')
+        if set_type == 'abs':
+            self._target_pos = value
+        else:
+            self._target_pos = self._position + value
+
+        self._init_value = self._position
+        if self._init_value != self._target_pos:
+
+
+            self._alpha = math.fabs(math.log(self._espilon / math.fabs(self._init_value - self._target_lambda)))
+        else:
+            self._alpha = math.fabs(math.log(self._espilon / 10))
+        self._start_time = perf_counter()
+        self._moving = True
+
+        def get_position(self):
+            """get the current position of the rotation actuator"""
+            return self._position
+
+        @property
+        def data_position(self, ):
+            return self._pos0
+
+
+        @data_position.setter
+        def data_position(self, pos0):
+            """Defines the center wavelength of the spectrum peak to be measured"""
+            if pos0 < 0:
+                raise ValueError('current mock rotation cannot be negative')
+            self._pos0 = pos0
             
